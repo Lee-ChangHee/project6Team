f7ca95a88d8c3024c5fd084dd0adda2ac869ea80
여수옥 <@DESKTOP-CLCVB11>
2021-02-06 23:40:37 +0900

    로그인 성공 실패 여부

--- src/LoginImpl.java
+++ src/LoginImpl.java
@@ -1,11 +1,26 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 
 public class LoginImpl implements ILogIn{
-
+	HashMap<String, MemberVO> store = new HashMap<String, MemberVO>();
 	@Override
-	public void Login(String id, String password) {
-		/*test*/
+	public boolean Login(String id, String password) {
+		
+		boolean bool;
+		if(store.containsKey(id)) {
+			System.out.println("로그인 성공");
+			bool = true;
+		}else {
+			System.out.println("로그인 실패");
+			bool = false;
+		}
+		return bool;
 	}
 
+	
 	@Override
 	public MemberVO findMyPage(String id) {
 	
