20c0b69d0a8a3b17c87a03240beefd3b6b982563
Lee-ChangHee <chbe5082@Naver.com>
2021-02-08 01:05:59 +0900

    Update Main MyPage Login 완성본

--- src/LoginImpl.java
+++ src/LoginImpl.java
@@ -1,21 +1,10 @@
 import java.util.HashMap;
-<<<<<<< HEAD
-=======
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
->>>>>>> refs/remotes/origin/master
 
 public class LoginImpl implements ILogIn{
 	HashMap<String, MemberVO> store = new HashMap<String, MemberVO>();
 	@Override
 	public boolean Login(String id, String password) {
-<<<<<<< HEAD
 
-=======
-		
->>>>>>> refs/remotes/origin/master
 		boolean bool;
 		if(store.containsKey(id)) {
 			System.out.println("로그인 성공");
@@ -27,11 +16,7 @@ public class LoginImpl implements ILogIn{
 		return bool;
 	}
 
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> refs/remotes/origin/master
 	@Override
 	public MemberVO findMyPage(String id) {
 
